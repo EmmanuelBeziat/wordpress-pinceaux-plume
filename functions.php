e59179a28ddb562b962485ad2eb471a62539ed8a
Emmanuel Béziat <contact@emmanuelbeziat.com>
2020-08-21 14:13:37 +0200

    test

--- functions.php
+++ functions.php
@@ -183,8 +183,8 @@ function plume_register_scripts() {
 		wp_enqueue_script('comment-reply');
 	}
 
-	wp_enqueue_script('plume-js', get_template_directory_uri() . '/assets/js/main.js', array(), $theme_version, false);
-	wp_script_add_data('plume-js', 'defer', true);
+	wp_enqueue_script('plume-js', get_template_directory_uri() . '/assets/js/main.min.js', array(), $theme_version, false);
+	wp_script_add_data('plume-js', 'async', true);
 
 }
 
