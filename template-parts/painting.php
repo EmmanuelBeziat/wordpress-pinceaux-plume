de674046fa023fce8f97e5ccde74f59d497a4aa8
Emmanuel Béziat <contact@emmanuelbeziat.com>
2020-08-25 01:34:56 +0200

    update front

--- template-parts/painting.php
+++ template-parts/painting.php
@@ -11,10 +11,12 @@
 ?>
 
 <a href="<?php echo get_permalink(the_ID()) ?>" <?php post_class('hp-paint hp-grid-item') ?> id="post-<?php the_ID(); ?>">
-  <figure class="paint-item">
+  <div class="hp-paint__image">
     <?php echo get_the_post_thumbnail(get_the_ID(), 'plume-painting-home'); ?>
-    <figcaption><?php echo get_the_title() ?></figcaption>
-  </figure>
+  </div>
+  <div class="hp-paint__caption">
+    <h2 class="hp-paint__title"><?php echo get_the_title() ?></h2>
+  </div>
   <?php
   // Single bottom post meta.
   // plume_the_post_meta(get_the_ID(), 'single-bottom');
