ec5d1c5f2c3d313c536f9a17bd61ac36c3a7df5c
Emmanuel Béziat <contact@emmanuelbeziat.com>
2020-08-26 15:49:12 +0200

    option page

--- functions.php
+++ functions.php
@@ -346,12 +346,34 @@ function plume_admin_bar_render() {
 }
 add_action('wp_before_admin_bar_render', 'plume_admin_bar_render');
 
-
+/*
 // Create options page
 function plume_add_options () {
 	add_theme_page('theme-options', 'Options', 'manage_options', 'plume-theme-options', 'plume_theme_options', null);
 }
-add_action('admin_menu', 'plume_add_options');
+add_action('admin_menu', 'plume_add_options'); */
+
+if (function_exists('acf_add_options_page')) {
+	acf_add_options_page([
+		'page_title' 	=> 'Theme General Settings',
+		'menu_title'	=> 'Theme Settings',
+		'menu_slug' 	=> 'theme-general-settings',
+		'capability'	=> 'edit_posts',
+		'redirect'		=> false
+	]);
+
+	acf_add_options_sub_page([
+		'page_title' 	=> 'Theme Header Settings',
+		'menu_title'	=> 'Header',
+		'parent_slug'	=> 'theme-general-settings',
+	]);
+
+	acf_add_options_sub_page([
+		'page_title' 	=> 'Theme Footer Settings',
+		'menu_title'	=> 'Footer',
+		'parent_slug'	=> 'theme-general-settings',
+	]);
+}
 
 // Manage Post and Page Admin Panel Columns
 add_action('manage_posts_custom_column', 'plume_show_post_thumbnail_column', 5, 2);
