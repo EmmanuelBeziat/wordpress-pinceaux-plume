189fc46c8cfe6fca155b70e60c0671f3ff89cb6f
Emmanuel Béziat <contact@emmanuelbeziat.com>
2020-09-02 05:49:42 +0200

    update with date

--- template-parts/painting.php
+++ template-parts/painting.php
@@ -23,6 +23,9 @@
   </div>
   <div class="hp-paint__caption">
     <h2 class="hp-paint__title"><?= get_the_title() ?></h2>
+    <?php if (get_field('date')) : ?>
+      <span class="hp-paint__date heading-size-6"><?= get_field('date') ?></span>
+    <?php endif ?>
 
     <?php if (get_field('paint-type')) : ?>
       <span class="hp-paint__type"><?= get_field('paint-type') ?></span>
