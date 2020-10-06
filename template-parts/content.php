00a7b15662d1631d85586df472c9004a6c00c9ba
Emmanuel Béziat <contact@emmanuelbeziat.com>
2020-10-06 02:47:33 +0200

    share button

--- template-parts/content.php
+++ template-parts/content.php
@@ -10,7 +10,6 @@
  * @subpackage Plume
  * @since Plume 1.0
  */
-
 ?>
 
 <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
@@ -48,38 +47,35 @@
 		<?php endif ?>
 	</div>
 
-
 	<div class="container">
 		<?php
-		wp_link_pages(
-			array(
-				'before'      => '<nav class="post-nav-links bg-light-background" aria-label="' . esc_attr__('Page', 'plume') . '"><span class="label">' . __('Pages:', 'plume') . '</span>',
-				'after'       => '</nav>',
-				'link_before' => '<span class="page-number">',
-				'link_after'  => '</span>',
-			)
-		);
+		wp_link_pages([
+			'before'      => '<nav class="post-nav-links bg-light-background" aria-label="Page"><span class="label">Pages</span>',
+			'after'       => '</nav>',
+			'link_before' => '<span class="page-number">',
+			'link_after'  => '</span>',
+		]);
 
 		// Single bottom post meta.
-		plume_the_post_meta(get_the_ID(), 'single-bottom');	?>
+		plume_the_post_meta(get_the_ID(), 'single-bottom');
+
+		// Share
+		get_template_part('template-parts/share')
+
+		// Navigation
+		if (is_single()) :
+			get_template_part('template-parts/navigation');
+		endif;
+
+		/**
+		 *  Output comments wrapper if it's a post, or if comments are open,
+		 * or if there's a comment number – and check for password.
+		 * */
+		if ((is_single() || is_page()) && (comments_open() || get_comments_number()) && !post_password_required()) : ?>
+			<div class="comments-wrapper">
+				<?php comments_template() ?>
+			</div>
+		<?php endif ?>
 	</div>
-
-	<?php
-	if (is_single()) : ?>
-		<div class="container">
-			<?php get_template_part('template-parts/navigation'); ?>
-		</div>
-	<?php endif ?>
-
-	<?php
-	/**
-	 *  Output comments wrapper if it's a post, or if comments are open,
-	 * or if there's a comment number – and check for password.
-	 * */
-	if ((is_single() || is_page()) && (comments_open() || get_comments_number()) && !post_password_required()) : ?>
-		<div class="comments-wrapper container">
-			<?php comments_template() ?>
-		</div>
-	<?php endif ?>
 
 </article>
---
+++ template-parts/share.php
@@ -0,0 +1,7 @@
+<div class="post-share">
+  <h2 class="heading-size-5">Partager</h2>
+
+  <div class="share-auto">
+    <button class="button" data-js=""><?php plume_the_theme_svg('share'); ?></button>
+  </div>
+</div>
