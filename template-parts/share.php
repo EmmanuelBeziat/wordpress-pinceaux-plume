0f7b2094ce2b9ac812ae5af83a6c42d4e62c1d67
Emmanuel Béziat <contact@emmanuelbeziat.com>
2020-10-06 03:23:22 +0200

    debug

--- template-parts/share.php
+++ template-parts/share.php
@@ -5,7 +5,7 @@
     <button class="button"><?php plume_the_theme_svg('share'); ?> Partager sur…</button>
   </div>
 
-  <div class="share-manual" hidden="hidden">
+  <div class="share-manual" hidden>
     <button class="button" data-name="twitter"><?php plume_the_theme_svg('twitter'); ?> Twitter</button>
     <button class="button" data-name="facebook"><?php plume_the_theme_svg('facebook'); ?> Facebook</button>
     <button class="button" data-name="link"><?php plume_the_theme_svg('link'); ?> Copier l’URL</button>
