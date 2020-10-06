5ca3663b0bafa9e38ab5d173bb585104343d1dcc
Emmanuel Béziat <contact@emmanuelbeziat.com>
2020-10-06 04:12:56 +0200

    share settings

--- inc/customize.php
+++ inc/customize.php
@@ -1,21 +1,36 @@
 <?php
 
+// Settings
+$capability = 'edit_theme_options';
+
 add_action('customize_register', function (WP_Customize_Manager $manager) {
+  // Sections
   $manager->add_section('plume_header', [
     'title' => 'En-tête'
   ]);
 
+  $manager->add_section('plume_post', [
+    'title' => 'Page peinture'
+  ]);
+
+  // Settings
   $manager->add_setting('header_background', [
     'default' => '',
-    'capability' => 'edit_theme_options'
+    'capability' => $capability
     // 'sanitize_callback' => saniti
   ]);
 
   $manager->add_setting('header_search', [
     'default' => '',
-    'capability' => 'edit_theme_options'
+    'capability' => $capability
   ]);
 
+  $manager->add_setting('post_share', [
+    'default' => '',
+    'capability' => $capability
+  ]);
+
+  // Control
   $manager->add_control(new WP_Customize_Image_Control($manager, 'header_background', [
     'section' => 'plume_header',
     'label' => 'Image de fond'
@@ -25,5 +40,11 @@ add_action('customize_register', function (WP_Customize_Manager $manager) {
     'label' => 'Afficher le champ de recherche',
     'type' => 'checkbox',
     'section' => 'plume_header'
+  ]);
+
+  $manager->add_control('post_share', [
+    'label' => 'Afficher la section de partage',
+    'type' => 'checkbox',
+    'section' => 'plume_post'
   ]);
 });
