b089610d5e93b8a9175c82061d06b85b6e60020b
Emmanuel Béziat <contact@emmanuelbeziat.com>
2020-10-06 03:18:08 +0200

    share module

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,6 +22,91 @@ class LazyGrid {
 	}
 }
 
+class Share {
+	constructor () {
+		this.postShare = document.querySelector('.post-share')
+		if (!this.postShare) return
+
+		navigator.share ? this.setShareAuto() : this.setShareManual()
+	}
+
+	setShareAuto () {
+		const shareBlock = this.postShare.querySelector('.share-auto')
+		if (!shareBlock) return
+
+		const shareButton = this.shareBlock.querySelector('button')
+		if (!shareButton) return
+
+		shareBlock.removeAttribute('hidden')
+		shareButton.addEventListener(click, event => {
+			event.preventDefault()
+			this.shareAuto()
+		})
+	}
+
+	setShareManual () {
+		const shareBlock = this.postShare.querySelector('.share-manual')
+		if (!shareBlock) return
+
+		const shareButtons = this.shareBlock.querySelectorAll('button')
+		if (!shareButtons.length) return
+
+		shareBlock.removeAttribute('hidden')
+		shareButtons.forEach(button, event => {
+			event.preventDefault()
+			this.shareManual(event.target.dataset.name)
+		})
+	}
+
+	shareAuto () {
+		navigator.share({
+			title: '',
+			text: document.title,
+			url: window.location.href,
+		})
+	}
+
+	shareManual (social) {
+		let shareUrl = ''
+		const pageUrl = encodeURIComponent(window.location.href)
+		const pageTitle = encodeURIComponent(document.title)
+
+		switch (social) {
+			case 'twitter':
+				shareUrl = `https://twitter.com/intent/tweet?text=${pageTitle} — ${pageUrl}&via=EmmanuelBeziat`
+				this.sharePopup(shareUrl, 'Partager sur Twitter')
+				break
+
+			case 'facebook':
+				shareUrl = `https://www.facebook.com/sharer/sharer.php?u=${pageUrl}`
+				this.sharePopup(shareUrl, 'Partager sur Facebook')
+				break
+
+			case 'link':
+				const dummyShare = document.createElement('input')
+				document.body.appendChild(dummyShare)
+				dummyShare.value = window.location.href
+				dummyShare.select()
+				document.execCommand('copy')
+				document.body.removeChild(dummyShare)
+				alert('URL copiée dans le presse papier')
+				break
+		}
+	}
+
+	sharePopup (url, title, width, height) {
+		let popupWidth = width || 640
+		let popupHeight = height || 320
+		let popupPosX = window.screenX + window.innerWidth / 2 - popupWidth / 2
+		let popupPosY = window.screenY + window.innerHeight / 2 - popupHeight / 2
+		let popup = window.open(url, title, 'scrollbars=yes, menubar=0, location=0, status=0, width=' + popupWidth + ', height=' + popupHeight + ', top=' + popupPosY + ', left=' + popupPosX)
+
+		popup.focus()
+		return true
+	}
+}
+
 document.addEventListener('DOMContentLoaded', () => {
 	new LazyGrid()
+	new Share()
 })
--- template-parts/share.php
+++ template-parts/share.php
@@ -1,7 +1,13 @@
 <div class="post-share">
   <h2 class="heading-size-5">Partager</h2>
 
-  <div class="share-auto">
-    <button class="button" data-js=""><?php plume_the_theme_svg('share'); ?></button>
+  <div class="share-auto" hidden>
+    <button class="button"><?php plume_the_theme_svg('share'); ?> Partager sur…</button>
+  </div>
+
+  <div class="share-manual" hidden="hidden">
+    <button class="button"><?php plume_the_theme_svg('twitter'); ?> Twitter</button>
+    <button class="button"><?php plume_the_theme_svg('facebook'); ?> Facebook</button>
+    <button class="button"><?php plume_the_theme_svg('link'); ?> Copier l’URL</button>
   </div>
 </div>
