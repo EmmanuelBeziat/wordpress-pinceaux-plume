1682705976c6aa680c3a86371dd1ee68153b1f05
Emmanuel Béziat <contact@emmanuelbeziat.com>
2023-09-25 18:28:18 +0200

    update

--- header.php
+++ header.php
@@ -17,7 +17,7 @@
 
 		<meta charset="<?php bloginfo('charset'); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" >
-		<meta name="description" content="<?php bloginfo('name') ?>">
+		<meta name="description" content="<?php bloginfo('description') ?>">
 
 		<link rel="profile" href="https://gmpg.org/xfn/11">
 
